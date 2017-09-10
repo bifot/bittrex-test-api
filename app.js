c6a67fe2f19e5c32b77498acf558aa9254dbe0f8
Mikhail Semin <bifot@bifot.ru>
2017-09-10 22:31:57 +0300

    refactor

--- app.js
+++ app.js
@@ -7,39 +7,39 @@ api.options({
 })
 
 api.getbalances((body, err) => {
-  if (err || !body.success) {
-    return console.error(err)
-  }
-
-  const items = body.result
-    .filter(item => item.Balance)
-    .map((item) => {
-      return new Promise((resolve, reject) => {
-        return api.getticker({
-          market: `BTC-${item.Currency}`
-        }, (body, err) => {
-          if (!err && body) {
-            resolve({
-              currency: item.Currency,
-              balance: item.Balance,
-              balanceBTC: item.Balance * body.result.Bid
-            })
-          } else {
-            reject(err)
-          }
+  if (!err && body.success) {
+    const items = body.result
+      .filter(item => item.Balance)
+      .map((item) => {
+        return new Promise((resolve, reject) => {
+          return api.getticker({
+            market: `BTC-${item.Currency}`
+          }, (body, err) => {
+            if (!err && body.success) {
+              resolve({
+                currency: item.Currency,
+                balance: item.Balance,
+                balanceBTC: item.Balance * body.result.Bid
+              })
+            } else {
+              reject(err)
+            }
+          })
         })
       })
-    })
-    // Deleting catch of promises for get the resolved data
-    .map(item => item.catch(() => undefined))
+      // Deleting catch of promises for get the resolved data
+      .map(item => item.catch(() => undefined))
 
-  return Promise.all(items)
-    .then((body) => {
-      const items = body.filter(item => item !== undefined)
-      const allCurrencies = items
-        .map(item => item.balanceBTC)
-        .reduce((a, b) => a + b)
+    return Promise.all(items)
+      .then((body) => {
+        const items = body.filter(item => item !== undefined)
+        const allCurrencies = items
+          .map(item => item.balanceBTC)
+          .reduce((a, b) => a + b)
 
-      console.log(`You have a ${allCurrencies} BTC.`)
-    })
+        console.log(`You have a ${allCurrencies} BTC.`)
+      })
+  } else {
+    console.error(err)
+  }
 })
